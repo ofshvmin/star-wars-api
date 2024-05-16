db4e106c8534f1e19de38cdb49b7550d98884f16
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-05-16 09:15:30 -0400

    add while loop to api call to retrieve all pages of results

--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -1,18 +1,33 @@
 
+// async function index () {
+//   try {
+//     let starships = []
+//     let next = 'https://swapi.dev/api/starships'
+//       const swApiResponse = await fetch(next)     
+//       const apiData = await swApiResponse.json()
+//       starships = [...starships, ...apiData.results]
+//         console.log(starships)
+//       next = apiData.next
+//       console.log(next)
+//     return starships
+//   } catch(err) {
+//     console.log(err)
+//   }
+// }
+
+
 async function index () {
   try {
     let starships = []
     let next = 'https://swapi.dev/api/starships'
+    while (next) {
       const swApiResponse = await fetch(next)     
       const apiData = await swApiResponse.json()
-
-
       starships = [...starships, ...apiData.results]
         console.log(starships)
-
       next = apiData.next
       console.log(next)
-    // }
+    }
     return starships
   } catch(err) {
     console.log(err)
